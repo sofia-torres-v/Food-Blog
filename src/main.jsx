42e425ac9067ca9eba36f566e4eff05317ba2586
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-01-05 13:35:54 -0500

    UPDATE:correction in home component names

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Post from "./pages/Post";
 import "./index.css";
 import "boxicons";
